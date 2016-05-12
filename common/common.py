5efaec7353b5179643bac40000a4dc14234544c9
exuyufe <led.xu@ericsson.com>
2016-05-12 16:48:31 +0800

    Add time operation

--- common/common.py
+++ common/common.py
@@ -6,7 +6,7 @@ Created on Feb 26, 2016
 
 common_functions = {
     'init_logger'         : lambda p1: init_logger(p1),
-    'get_formatted_time'  : lambda : print_cur_time(),
+    'get_formatted_time'  : lambda : print_time(),
     'parse_xml'           : lambda p1: xml_parser(p1),
     'run_local_cmd'       : lambda p1 : run_local_cmd(p1),
     'run_remote_cmd'      : lambda p1,p2,p3,p4,p5 : run_remote_cmd(p1, p2, p3, p4, p5),
@@ -24,11 +24,43 @@ def init_logger(file_path):
                 filename='%s' %file_path,
                 filemode='w')
 
-import time    
-def print_cur_time():
+import time
+import datetime 
+from dateutil import tz
+def print_time():
     '''
     '''
-    print time.strftime('%Y%m%d_%H%M%S', time.localtime(time.time()))
+    #get current timestamp using time, has microsecond-level accuracy 
+    t_cur = time.time() # 1463039645.589426
+
+    #get current time using datetime, has microsecond-level accuracy
+    dt_cur = datetime.datetime.now() # datetime.datetime(2016, 5, 12, 15, 51, 12, 699969)
+
+    #string -> datetime
+    s1 = '2015-11-12 10:34:34.34431'
+    dt1 = datetime.datetime.strptime(s1, '%Y-%m-%d %H:%M:%S.%f') # datetime.datetime(2015, 11, 12, 10, 34, 34, 344310)
+    ms1 = dt1.microsecond
+
+    #datetime -> string
+    s2 = dt1.strftime('%Y=%m=%d %f') # '2015=11=12 344310'
+ 
+    #datetime -> timestamp, will only have second-level accuracy
+    ts3 = time.mktime(dt1.timetuple()) # 1447295674.0
+
+    #timestamp -> UTC time -> string
+    ut4 = time.gmtime(ts3)    # time.struct_time(tm_year=2015, tm_mon=11, tm_mday=12, tm_hour=2, tm_min=34, tm_sec=34, tm_wday=3, tm_yday=316, tm_isdst=0)
+    s4 = time.strftime('%Y%m%d_%H%M%S', ut4) # '20151112_023434'
+ 
+    #timestamp -> Local time -> string
+    lt5 = time.localtime(ts3) # time.struct_time(tm_year=2015, tm_mon=11, tm_mday=12, tm_hour=10, tm_min=34, tm_sec=34, tm_wday=3, tm_yday=316, tm_isdst=0)
+    s5 = time.strftime('%Y%m%d_%H%M%S', lt5) # '20151112_103434'
+
+    #change datetime from local -> UTC using dateutil
+    from_zone = tz.tzutc()
+    to_zone = tz.tzlocal()
+    ldt6 = dt1.replace(tzinfo=from_zone) # datetime.datetime(2011, 1, 21, 2, 37, 21, tzinfo=tzlocal())
+    udt6 = ldt6.astimezone(to_zone)      # datetime.datetime(2011, 1, 20, 18, 37, 21, tzinfo=tzutc())
+    
 
 import xml.etree.ElementTree as ET
 def xml_parser(file_path='common-xml.xml'):    
@@ -182,14 +214,28 @@ def multi_thread_run3(op, params):
     cost = time.time() - cur
     print 'MultiThreadRun2 total time cost: %s' %cost
     
+def normalise_ipv6(ip):
+    '''
+    Build a complete-format ipv6 address
+    '''
+    colons = ip.count(':')
+    if colons == 0:
+        return ip
+    if colons < 7:
+        ip = ip.replace('::', ':'*(9-colons))
+    ip = ':'.join([i.zfill(4) for i in ip.split(':')])
+    return ip.upper()
 
 import re
 def validate_ipv6(ipv6):
     '''
-    check if the input ip is a valid IPV6 address. Exception will be raised if it's invalid
+    check if the input ip is a valid IPV6 address. Exception will be raised if it's invalid. 
+    For example 'fe80::20c:9ff:fe2c:a00'
     '''
-    pt = r'^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$'
-    print re.match(pt, ipv6, re.M|re.I).group()
+    pt_perfect = r'^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$'
+    pt_poor = r'(([1-9]?[0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([1-9]?[0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
+    pt_poor2 = r'(([0-9A-Fa-f]{1,4}(:[0-9A-Fa-f]{1,4})*)?)::(([0-9A-Fa-f]{1,4}(:[0-9A-Fa-f]{1,4})*)?)(%[^%\s]+)?'
+    print re.match(pt_perfect, ipv6, re.M|re.I).group()
 
 
 if __name__ == '__main__':
